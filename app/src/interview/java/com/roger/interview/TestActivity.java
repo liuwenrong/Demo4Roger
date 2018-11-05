9afda9b0ebbf4c20fd3ace44a8c855c7e4cdac8a
liuwenrong <liuwenrongRoger@126.com>
2018-11-05 14:22:04 +0800

    增加通过USB复制电脑内容到手机的功能
    Signed-off-by: liuwenrong <liuwenrongRoger@126.com>

--- app/src/interview/java/com/roger/interview/TestActivity.java
+++ app/src/interview/java/com/roger/interview/TestActivity.java
@@ -1,8 +1,16 @@
 package com.roger.interview;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.roger.demo4roger.R;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -18,15 +26,23 @@ public class TestActivity extends AppCompatActivity {
     }
 
     private static final String TAG = "Test";
-
-
+    EditText mEditCopy;//用于手机复制黏贴
+    String text = "https://ptsolomo.reader.qq.com/book_res/reader/eink/1_0_0/topicV2.html?tid=302953&tf=1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        mEditCopy = findViewById(R.id.edit_copy);
+        mEditCopy.setText(text);
         main(new String[]{});
         Log.d(TAG, "onCreate: ----------------------------180326");
         String DeviceName = android.os.Build.DEVICE;
         Log.d(TAG, "onCreate:----------180326 DeviceName = " + DeviceName);
+    }
+
+    public void onClickCopy(View view) {
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboardManager.setPrimaryClip(ClipData.newPlainText(null, mEditCopy.getText()));
+        Toast.makeText(this, "已复制到剪切板", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/roger/demo4roger/MainActivity.java
+++ app/src/main/java/com/roger/demo4roger/MainActivity.java
@@ -1,7 +1,7 @@
 package com.roger.demo4roger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,4 +16,5 @@ public class MainActivity extends AppCompatActivity {
         String DeviceName = android.os.Build.DEVICE;
         Log.d(TAG, "onCreate:----------180326 DeviceName = " + DeviceName);
     }
+
 }
